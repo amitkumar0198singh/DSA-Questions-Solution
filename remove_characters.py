55a747923e2c1a5dfb44cc57f54b69738414ae86
amitkumar0198singh <114804239+amitkumar0198singh@users.noreply.github.com>
2024-01-16 19:23:04 +0530

    Add files via upload

---
+++ remove_characters.py
@@ -0,0 +1,22 @@
+'''
+#2 Remove Character
+Given two strings string1 and string2, remove those characters from first string(string1) 
+which are present in second string(string2).
+Topic: Array
+Level: Easy
+'''
+
+
+# Python3 program to remove common characters from two strings
+def remove_common_characters(str1, str2):
+    result = ""
+
+    for char in str1:
+        if char not in str2:
+            result += char
+    return result 
+
+# def remove_common_characters(string1, string2):
+#     return "".join([char for char in string1 if char not in string2])
+
+print(remove_common_characters("Amit", "Ameet"))
